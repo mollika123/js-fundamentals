6f6fc81572061a31c6bba5c503317c9694e860ed
mollika123 <aktermollika49@gmail.com>
2026-09-11 17:00:41 +0200

    break emplement

---
+++ break.js
@@ -0,0 +1,9 @@
+// যখন break আসে, তখন loop সেখানেই শেষ হয়ে যায়।
+
+for (let i = 0; i < 10;i++
+){
+  if (i === 5) {
+    break;
+  }
+  console.log(i);
+}
--- loop.js
+++ loop.js
@@ -1,3 +1,11 @@
-for (let i = 0; i < 50; i++){
-  console.log('Love you ma');
+// for (let i = 0; i < 50; i++){
+//   console.log('Love you ma');
+// }
+
+// while lop
+
+let i = 0;
+while (i < 30) {
+  console.log("love you ma");
+  i++;
 }
